8e2d80357ee7baf3ab5c226c582c9809cb34035f
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-09-29 18:32:55 +1000

    Add a function to determine result based on score

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,14 +4,23 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-# Fixed code as below
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get score from user and determine result."""
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+def determine_result(score):
+    """Determine result based on score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
+
