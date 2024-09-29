2ba43ff8ae564bc8aa614dfa14206af320384085
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-09-29 20:48:11 +1000

    Add main() function with menu loop, and functions to get a valid score, determine result based on score, and print stars based on score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -39,3 +39,50 @@ function print_stars(score)
     for each score in score
         print *
 """
+
+
+def main():
+    """Print menu and print results based on option."""
+    menu = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
+    print(menu)
+    option = input(">>> ").upper()
+    while option != "Q":
+        if option == "G":
+            score = int(input("Enter your score: "))
+            score = get_valid_score(score)
+        elif option == "P":
+            result = determine_result(score)
+            print(result)
+        elif option == "S":
+            print_stars(score)
+            print()
+        print(menu)
+        option = input(">>> ").upper()
+    print("Thanks for playing!")
+
+def get_valid_score(score):
+    """Get a valid score between 0 and 100 inclusive."""
+    while score < 0 or score > 100:
+        print("Score must be between 0 and 100")
+        score = int(input("Enter your score: "))
+    return score
+
+def determine_result(score):
+    """Determine result based on score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+def print_stars(score):
+    """Print stars based on score."""
+    for i in range(score):
+        print("*", end="")
+
+main()
+
+
