27d042c3372a45bc6ae5e6d758713bf3d876b3c6
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-03 12:07:24 +1000

    Add responses to questions as per instructions

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+# Line 1:
+# Output: 8
+# Smallest possible is 5, largest possible is 20.
+
+# Line 2:
+# Output: 3
+# Smallest possible is 3, largest possible is 9.
+
+# Line 3:
+# Output: 3.7823013150878975
+# Smallest possible is 2.5, largest possible is 5.5
+
+# Question 4 Code:
+
+import random
+print(random.randint(1, 100))
+
