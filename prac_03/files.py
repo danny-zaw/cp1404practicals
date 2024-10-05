b57a731efa0604fe9618693624f91ce777cd41a6
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-05 22:50:57 +1000

    Add code for question 4

--- prac_03/files.py
+++ prac_03/files.py
@@ -15,6 +15,14 @@ in_file.close()
 # Question 3
 
 with open("numbers.txt") as in_file:
-    first_number = int(in_file.readline())
-    second_number = int(in_file.readline())
+    first_number = int(in_file.readline().strip())
+    second_number = int(in_file.readline().strip())
     print(first_number+second_number)
+
+# Question 4
+total = 0
+with open("numbers.txt") as in_file:
+    for line in in_file:
+        number = int(line.strip())
+        total += number
+    print(total)
