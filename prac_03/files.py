a66ac2b8a89043073a23fc9088d5a62736c7b739
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-05 22:41:51 +1000

    Add code for question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,3 +11,10 @@ in_file = open("name.txt")
 name_from_file = in_file.read().strip()
 print(f"Hi {name_from_file}!")
 in_file.close()
+
+# Question 3
+
+with open("numbers.txt") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+    print(first_number+second_number)
