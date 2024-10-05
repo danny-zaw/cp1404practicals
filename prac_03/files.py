0bc4fab2ddc3cc3c2a53fb4b96b2c0c5e91416bf
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-05 22:35:17 +1000

    Add code for question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,10 @@ out_file = open("name.txt", "w")
 name = input("Enter your name: ")
 print(name, file=out_file)
 out_file.close()
+
+# Question 2
+
+in_file = open("name.txt")
+name_from_file = in_file.read().strip()
+print(f"Hi {name_from_file}!")
+in_file.close()
