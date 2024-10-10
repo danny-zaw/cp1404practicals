f4de0786474362164664801b73e515e11f89a9e0
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-10 14:31:37 +1000

    Modify load subject data function to return data as nested lists and add new function to display subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,25 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subject_data = load_subject_data()
+    print_subject_details(subject_data)
 
 
-def load_data():
+def load_subject_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
 
+def print_subject_details(subject_data):
+    """Print subject details with subject code, lecturer and number of students."""
+    for data in subject_data:
+        print("{} is taught by {} and has {} students.".format(*data))
 
 main()
