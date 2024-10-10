7f51a98d035fb0bcbb9715eac4bdffe6ecf46997
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-10 13:23:41 +1000

    Add responses to questions as per practical instructions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,32 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# PART 1
+
+# 1. 3
+# 2. 2
+# 3. 1
+# 4. [3, 1, 4, 1, 5, 9]
+# 5. [1]
+# 6. True
+# 7. False
+# 8. False
+# 9. [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# PART 2
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+
+numbers[0] = "ten"
+
+# 2. Change the last element of numbers to 1
+
+numbers[-1] = 1
+
+# 3. Print all the elements from numbers except the first two (slice)
+
+print(numbers[2:])
+
+# 4. Print whether 9 is an element of numbers
+
+print(9 in numbers)
+
