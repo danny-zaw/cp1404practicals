94c12031da2eda2e07758f394b45ff7fd85a8413
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-13 17:01:28 +1000

    Refactor variable names

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -30,11 +30,12 @@ print(a_names)
 print(" ".join(sorted(names)))
 
 # TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [full_name.lower() for full_name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+numbers =
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
