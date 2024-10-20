40056eb879266ef08520838d12febe76a17444ff
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-10-20 22:24:47 +1000

    Complete prac 5 emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,4 +1,28 @@
 """
-
-
+CP1404 - Prac 5
+Program: Emails
 """
+
+def main():
+    """Create dictionary of emails to names, and displays it."""
+    email_to_name = {}
+    email = input('Email: ')
+
+    while email != "":
+        name = extract_name(email)
+        name_confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+        if name_confirmation != "" and name_confirmation != "y":
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input('Email: ')
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name(email):
+    """Extract name from email."""
+    unformatted_name = email.split('@')[0]
+    name = " ".join(unformatted_name.split('.')).title()
+    return name
+
+main()
