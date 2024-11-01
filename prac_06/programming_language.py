eb04a35586e84c847bf90287a0de9cb6a74a7309
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-11-01 21:09:38 +1000

    Correct code for is_dynamic() method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,17 +7,17 @@ Actual   :
 
 class ProgrammingLanguage:
 
-    def __init__(self, language, typing, reflection, year):
+    def __init__(self, language_name, typing, reflection, year):
         """Initialise instances for language, language, reflection and year."""
-        self.language = language
+        self.language_name = language_name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
         """Return true if language is dynamic."""
-        return self.is_dynamic().lower == "dynamic"
+        return self.typing.lower() == "dynamic"
 
     def __str__(self):
         """Return string representation of language."""
-        return f"{self.language}, {self.typing} Typing, Reflection {self.reflection}, First appeared in {self.year}"
+        return f"{self.language_name}, {self.typing} Typing, Reflection {self.reflection}, First appeared in {self.year}"
