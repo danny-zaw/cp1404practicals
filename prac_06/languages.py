43d45ed75ddccb2fec9a19ef8877dc645fc5c032
dannyzaw <danny.zaw@my.jcu.edu.au>
2024-11-01 20:56:12 +1000

    Import class and copy code from prac instructions

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Practical 5
+Program : Languages
+Estimate: 20 minutes
+Actual  :
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
